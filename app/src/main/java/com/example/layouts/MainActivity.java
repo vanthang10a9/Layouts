e0870d4adb1d8fbfc40ac82a1ded54d69f0c3b01
vanthang10a9 <levanthang510@gmail.com>
2019-08-26 20:57:41 +0700

    tạo frame layout, - frame layout có ghi đè view lên nhau, -set nút ẩn hiện imageview nền che textview nằm sau

--- app/src/main/java/com/example/layouts/MainActivity.java
+++ app/src/main/java/com/example/layouts/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.layouts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView mImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageView = (ImageView) findViewById(R.id.image);
+
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mImageView.getVisibility() == mImageView.VISIBLE)
+                    mImageView.setVisibility(View.GONE);
+                else
+                    mImageView.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
